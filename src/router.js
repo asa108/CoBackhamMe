d46affaabbe56085037a6e22a1d9a130ba15386b
MASAHIRO KUROKAWA <masahiro819@gmail.com>
2020-11-13 21:33:21 +0900

    routing test

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "./pages/Home";
-import Works from "./pages/Works";
+import Home from "./pages/Home.vue";
+import Works from "./pages/Works.vue";
+import Test from "./pages/Test.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", component: Home },
+    { path: "/test", component: Test },
     { path: "/works", component: Works }
   ]
 });
